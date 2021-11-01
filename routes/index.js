c0a6cd1808f8b13ac045ac0e69d0161882d8390b
ayans101 <78583131+ayans101@users.noreply.github.com>
2021-11-01 13:48:57 +0530

    set up controller

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const router = express.Router();
+const testController = require('../controllers/test_controller');
 
 console.log('router loaded');
+
+router.get('/', testController.home);
 
 module.exports = router;
