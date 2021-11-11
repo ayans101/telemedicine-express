eacff42b25bb78ed90e7d44eb22b0ffaadf7e274
ayans101 <78583131+ayans101@users.noreply.github.com>
2021-11-12 01:18:39 +0530

    update appointment schema

--- models/appointment.js
+++ models/appointment.js
@@ -1,6 +1,10 @@
 const mongoose = require('mongoose');
 
 const appointmentSchema = new mongoose.Schema({
+    creator: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+    },
     attendees: [
         {
             type: mongoose.Schema.Types.ObjectId,
