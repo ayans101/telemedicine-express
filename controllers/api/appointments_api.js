c10bbd23ebebefedf50d20f4c6c325b185399af0
ayans101 <78583131+ayans101@users.noreply.github.com>
2021-11-12 02:12:00 +0530

    add api to create a new appointment

--- controllers/api/appointments_api.js
+++ controllers/api/appointments_api.js
@@ -1,11 +1,38 @@
 const Appointment = require('../../models/appointment');
 const User = require('../../models/user');
 
-module.exports.createAppointment = function(req, res) {
-    return res.status(200).json({
-        message: "Hello World",
-        success: true
-    });
+module.exports.createAppointment = async function(req, res) {
+    try {
+
+        let creator = await User.findById(req.body.creator);
+        if(!creator || creator.userType !== 'Doctor') {
+            return res.status(404).json({
+                message: "User not found or User is not a Doctor",
+                success: false,
+            });
+        }
+        await Appointment.create(req.body, async function(err, appointment) {
+            for(uid of appointment.attendees) {
+                let attendee = await User.findById(uid);
+                attendee.futureAppointment.push(appointment);
+                attendee.save();
+            }
+            return res.status(200).json({
+                message: "Appointment Created Successfully",
+                success: true,
+                data: {
+                    appointment: appointment
+                }
+            });
+        });
+
+    } catch {
+        console.log(err);
+        return res.status(500).json({
+            message: "Internal Server Error",
+            success: false
+        });  
+    }
 }
 
 module.exports.updateAppointment = function(req, res) {
