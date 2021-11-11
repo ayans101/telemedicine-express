4fd2db4179135a59e198dbf248d79ca3d5fd19a9
ayans101 <78583131+ayans101@users.noreply.github.com>
2021-11-12 01:14:56 +0530

    allow cross-origin requests

--- index.js
+++ index.js
@@ -5,7 +5,9 @@ const db =require('./config/mongoose');
 const User = require('./models/user');
 const passport = require('passport');
 const passportJWT = require('./config/passport-jwt-strategy');
+const cors = require('cors');
 
+app.use(cors()); 
 app.use(express.urlencoded({ extended: true }));
 app.use(passport.initialize());
 
