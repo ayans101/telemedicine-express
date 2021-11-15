28c0c1c27dc4be030944e2a2a2e5991b0612101c
ayans101 <78583131+ayans101@users.noreply.github.com>
2021-11-15 23:26:54 +0530

    minor changes and fixes

--- controllers/api/users_api.js
+++ controllers/api/users_api.js
@@ -1,12 +1,14 @@
 const User = require('../../models/user');
 const env = require('../../config/environment');
 const jwt = require('jsonwebtoken');
+/*
 const { db } = require('../../config/environment');
 const accountSid = "ACac7e7a383e47c6ea4a3014c64b88a914";
 const authToken = "b43bbb70bd3764de595dd416cb546ae1";
 const client = require('twilio')(accountSid, authToken);
+*/
 
-module.exports.createSession = async function(req, res){
+module.exports.login = async function(req, res){
     try{
         let user = await User.findOne({email: req.body.email});
         if(!user || user.password != req.body.password){
@@ -38,47 +40,40 @@ module.exports.createSession = async function(req, res){
     }
 }
 
-module.exports.registerUser = function(req, res){
+module.exports.register = async function(req, res){
+    try {
+        let user = await User.findOne({email: req.body.email});
+        if(user) {
+            return res.status(422).json({
+                message: "There already exists an account registered with this email address",
+                success: false
+            });
+        }
+        await User.create(req.body, function(err, user){
+            return res.status(200).json({
+                message: "Sign up successful, here is your token, please keep it safe",
+                success: true,
+                data: {
+                    token: jwt.sign(user.toJSON(), env.jwt_secret, {expiresIn: '1d'}),
+                    user: {
+                        email: user.email,
+                        userType: user.userType,
+                        name: user.name,
+                        age: user.age,
+                        phoneNumber: user.phoneNumber,
+                        _id: user._id,
+                    }
+                }
+            });
+        });
 
-    if(req.body.password != req.body.confirm_password){
-        return res.status(400).json({
-            message: "The passwords entered do not match",
+    } catch(err){
+        console.log(err);
+        return res.status(500).json({
+            message: "Internal Server Error",
             success: false
         });
-    }else{
-        User.findOne({email: req.body.email}, function(err, user){
-            let otp = 10000;
-            if(!user){
-                User.create(req.body, function(err, user){
-                    return res.status(200).json({
-                        message: "Sign up successful, here is your token, please keep it safe",
-                        success: true,
-                        data: {
-                            token: jwt.sign(user.toJSON(), env.jwt_secret, {expiresIn: '1d'}),
-                            user: {
-                                email: user.email,
-                                userType: user.userType,
-                                name: user.name,
-                                age: user.age,
-                                phoneNumber: user.phoneNumber,
-                                _id: user._id,
-                                otp: otp
-                            }
-                        }
-                    });
-                });
-                client.messages
-                    .create({body: otp, from: '+15017122661', to: user.phoneNumber})
-                    .then(message => console.log(message.sid));
-                //RETURN TO A PAGE FOR ENTERING THE OTP
-            }else{
-                return res.status(422).json({
-                    message: "There already exists an account registered with this email address",
-                    success: false
-                });
-            }
-        });   
-    }
+    }   
 }
 
 module.exports.profile = async function(req, res){
@@ -115,7 +110,21 @@ module.exports.profile = async function(req, res){
     }
 };
 
+module.exports.generateOTP = async function(req, res) {
+    try {
+        
+        // TODO
 
+    } catch {
+        console.log(err);
+        return res.status(500).json({
+            message: "Internal Server Error",
+            success: false
+        });
+    }
+}
+
+/*
 module.exports.verifyOTP = function (req, res) {
     models.users.findOne({email: req.body.email}, function (err, user) {
         if (err) {
@@ -137,3 +146,4 @@ module.exports.verifyOTP = function (req, res) {
         }
     })
 }
+*/
--- routes/api/users.js
+++ routes/api/users.js
@@ -3,8 +3,9 @@ const router = express.Router();
 const passport = require('passport');
 const usersApi = require('../../controllers/api/users_api');
 
-router.post('/create-session', usersApi.createSession);
-router.post('/register-user', usersApi.registerUser);
-router.post('/otp-verify', usersApi.verifyOTP);
+router.post('/login', usersApi.login);
+router.post('/register', usersApi.register);
 router.get('/:id', passport.authenticate('jwt', {session: false}), usersApi.profile);
+// router.post('/verify-otp', usersApi.verifyOTP);
+// router.post('/generate-otp', usersApi.generateOTP);
 module.exports = router;
