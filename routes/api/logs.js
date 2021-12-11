29d08098a53e326e2ebe9c9b4e42d2fae634d6c9
sneha-nitdgp <sc.19u10416@btech.nitdgp.ac.in>
2021-12-11 20:27:24 +0530

    added log-apis

--- routes/api/logs.js
+++ routes/api/logs.js
@@ -4,5 +4,6 @@ const passport = require('passport');
 const logsApi = require('../../controllers/api/logs_api');
 
 router.post('/create', passport.authenticate('jwt', {session: false}), logsApi.create);
-
+router.get('/all', passport.authenticate('jwt', {session: false}), logsApi.all);
+router.get('/read/:id',  passport.authenticate('jwt', {session: false}), logsApi.read);
 module.exports = router;
