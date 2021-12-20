9cbab78346f323be54a1a18c77d028469334c0c8
Shouvit Pradhan <shaw8wit@gmail.com>
2021-12-20 22:00:35 +0530

    minor changes

--- controllers/api/users_api.js
+++ controllers/api/users_api.js
@@ -1,134 +1,130 @@
-const User = require('../../models/user');
-const env = require('../../config/environment');
-const jwt = require('jsonwebtoken');
-const bcrypt = require('bcrypt');
+const User = require("../../models/user");
+const env = require("../../config/environment");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
 
-module.exports.login = async function(req, res){
-    try{
-        let user = await User.findOne({email: req.body.email});
-        let isPasswordMatched = await bcrypt.compare(req.body.password, user.password);
-        if(!user || !isPasswordMatched){
-            return res.status(422).json({
-                message: "Invalid username or password",
-                success: false
-            });
-        }
-
-        return res.status(200).json({
-            message: "Sign in successful, here is your token, please keep it safe",
-            success: true,
-            data: {
-                token: jwt.sign(user.toJSON(), env.jwt_secret, {expiresIn: '1d'}),
-                user: {
-                    name: user.name,
-                    email: user.email,
-                    _id: user._id
-                }
-            }
-        });
-
-    }catch(err){
-        console.log(err);
-        return res.status(500).json({
-            message: "Internal Server Error",
-            success: false
-        });
+module.exports.login = async function (req, res) {
+  try {
+    let user = await User.findOne({ email: req.body.email });
+    let isPasswordMatched = await bcrypt.compare(
+      req.body.password,
+      user.password
+    );
+    if (!user || !isPasswordMatched) {
+      return res.status(422).json({
+        message: "Invalid username or password",
+        success: false,
+      });
     }
-}
 
-module.exports.register = async function(req, res){
-    try {
-        let user = await User.findOne({email: req.body.email});
-        if(user) {
-            return res.status(422).json({
-                message: "There already exists an account registered with this email address",
-                success: false
-            });
-        }
-        const salt = await bcrypt.genSalt(8);
-        const hashedPassword = await bcrypt.hash(req.body.password, salt);
-        req.body.password = hashedPassword;
-        await User.create(req.body, function(err, user){
-            return res.status(200).json({
-                message: "Sign up successful, here is your token, please keep it safe",
-                success: true,
-                data: {
-                    token: jwt.sign(user.toJSON(), env.jwt_secret, {expiresIn: '1d'}),
-                    user: {
-                        email: user.email,
-                        userType: user.userType,
-                        name: user.name,
-                        age: user.age,
-                        phoneNumber: user.phoneNumber,
-                        _id: user._id,
-                    }
-                }
-            });
-        });
+    return res.status(200).json({
+      message: "Sign in successful, here is your token, please keep it safe",
+      success: true,
+      data: {
+        token: jwt.sign(user.toJSON(), env.jwt_secret, { expiresIn: "1d" }),
+        user: {
+          name: user.name,
+          email: user.email,
+          _id: user._id,
+        },
+      },
+    });
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json({
+      message: "Internal Server Error",
+      success: false,
+    });
+  }
+};
 
-    } catch(err){
-        console.log(err);
-        return res.status(500).json({
-            message: "Internal Server Error",
-            success: false
-        });
-    }   
-}
-
-
-module.exports.profile = async function(req, res){
-    try{
-        let user = await User.findById(req.params.id);
-        if(!user){
-            return res.status(404).json({
-                message: "User not found",
-                success: false,
-            });
-        }else{
-            return res.status(200).json({
-                message: "User Details",
-                success: true,
-                data: {
-                    user: {
-                        email: user.email,
-                        userType: user.userType,
-                        name: user.name,
-                        age: user.age,
-                        phoneNumber: user.phoneNumber,
-                        _id: user._id
-                    }
-                }
-            });
-        }
-
-    }catch(err){
-        console.log(err);
-        return res.status(500).json({
-            message: "Internal Server Error",
-            success: false
-        });
+module.exports.register = async function (req, res) {
+  try {
+    let user = await User.findOne({ email: req.body.email });
+    if (user) {
+      return res.status(422).json({
+        message:
+          "There already exists an account registered with this email address",
+        success: false,
+      });
     }
+    const salt = await bcrypt.genSalt(8);
+    const hashedPassword = await bcrypt.hash(req.body.password, salt);
+    req.body.password = hashedPassword;
+    await User.create(req.body, function (err, user) {
+      return res.status(200).json({
+        message: "Sign up successful, here is your token, please keep it safe",
+        success: true,
+        data: {
+          token: jwt.sign(user.toJSON(), env.jwt_secret, { expiresIn: "1d" }),
+          user: {
+            email: user.email,
+            userType: user.userType,
+            name: user.name,
+            age: user.age,
+            phoneNumber: user.phoneNumber,
+            _id: user._id,
+          },
+        },
+      });
+    });
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json({
+      message: "Internal Server Error",
+      success: false,
+    });
+  }
+};
+
+module.exports.profile = async function (req, res) {
+  try {
+    let user = await User.findById(req.params.id);
+    if (!user) {
+      return res.status(404).json({
+        message: "User not found",
+        success: false,
+      });
+    } else {
+      return res.status(200).json({
+        message: "User Details",
+        success: true,
+        data: {
+          user: {
+            email: user.email,
+            userType: user.userType,
+            name: user.name,
+            age: user.age,
+            phoneNumber: user.phoneNumber,
+            _id: user._id,
+          },
+        },
+      });
+    }
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json({
+      message: "Internal Server Error",
+      success: false,
+    });
+  }
 };
 
 module.exports.getAllDoctors = async function (req, res) {
-    try {
-        let doctors = await User.find({ userType: 'Doctor' });
-        for(doctor of doctors) {
-            doctor.password = null;
-        }
-        return res.status(200).json({
-            message: "Doctor details Retrieved",
-            success: true,
-            data: {
-                doctors: doctors
-            }
-        });
-  
-    } catch(err) {
-        console.log(err);
-        return res.status(500).json({
-            message: "Internal Server Error",
-            success: false
-        });  
-    }
+  try {
+    let doctors = await User.find({ userType: "Doctor" }).select("name");
+    return res.status(200).json({
+      message: "Doctor details Retrieved",
+      success: true,
+      data: {
+        doctors: doctors,
+      },
+    });
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json({
+      message: "Internal Server Error",
+      success: false,
+    });
   }
+};
--- routes/api/users.js
+++ routes/api/users.js
@@ -1,10 +1,19 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const passport = require('passport');
-const usersApi = require('../../controllers/api/users_api');
+const passport = require("passport");
+const usersApi = require("../../controllers/api/users_api");
 
-router.post('/login', usersApi.login);
-router.post('/register', usersApi.register);
-router.get('/:id', passport.authenticate('jwt', {session: false}), usersApi.profile);
-router.post('/all-doctors', passport.authenticate("jwt", { session: false }), usersApi.getAllDoctors);
+router.post("/login", usersApi.login);
+router.post("/register", usersApi.register);
+router.get(
+  "/all-doctors",
+  passport.authenticate("jwt", { session: false }),
+  usersApi.getAllDoctors
+);
+router.get(
+  "/:id",
+  passport.authenticate("jwt", { session: false }),
+  usersApi.profile
+);
+
 module.exports = router;
